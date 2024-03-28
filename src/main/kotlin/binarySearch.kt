1ea577deb14a234a0d4f890663ee7207a8806951
Franciscoevora <frevora7905@gmail.com>
2024-03-28 12:37:12 +0000

    factorial

--- src/main/kotlin/binarySearch.kt
+++ src/main/kotlin/binarySearch.kt
@@ -1,9 +1,9 @@
-fun binarySearch(a:IntArray, left:Int, right:Int, elem:Int):Int{
-    if (left>right) return -1
-    val mid = (left+right)/2
-    return when{
-        a[mid] == elem -> mid
-        a[mid] > elem -> binarySearch(a,left,mid-1,elem)
-        else -> binarySearch(a,left, mid+1, elem)
+fun binarySearch(a: IntArray, l: Int, r: Int, elem: Int): Int {
+    if(r < l) return -1
+    val mid = (l+r)/2
+    return when {
+        a[mid]==elem -> mid
+        a[mid]>elem -> binarySearch(a, l, mid - 1, elem)
+        else -> binarySearch(a, mid+1, r, elem)
     }
 }
--- src/main/kotlin/problemas.kt
+++ src/main/kotlin/problemas.kt
@@ -5,10 +5,10 @@ fun countPairsThatSumN(v: IntArray, l: Int, r: Int, s: Int): Int{
     var left = l
     var right = r
     while (left < right){
-        val count = v[right] + v[left]
-        if (count < s) ++left
-        if (count > s) --right
-        if (count == s){
+        val j = v[right] + v[left]
+        if (j < s) ++left
+        if (j > s) --right
+        if (j == s){
             p++
             left++
             right--
@@ -19,7 +19,7 @@ fun countPairsThatSumN(v: IntArray, l: Int, r: Int, s: Int): Int{
 
 // exercicio 2
 
-// 2.1
+// 2.1 O(n^3)
 fun countEachThreeElementsThatSumN21(v: IntArray, l: Int, r: Int, s: Int): Int{
     var p = 0
     for (i in l..r - 2){
@@ -32,18 +32,50 @@ fun countEachThreeElementsThatSumN21(v: IntArray, l: Int, r: Int, s: Int): Int{
     return p
 }
 
-//2.2
+//2.2 O(n^2logn)
 fun countEachThreeElementsThatSumN22(v: IntArray, l: Int, r: Int, s: Int): Int{
+    var p = 0
+    mergeSort(v,l,r)
+    for (i in l .. r - 2){
+        for (j in l + 1 .. r - 1){
+            val k = s - v[i] - v[j]
+            if (k < v[j]) break
+            val c = binarySearch(v, j + 1, r, k)
+            if (c != -1) p++
+        }
+    }
+    return p
+}
 
+//2.3 O(n^2)
+
+
+fun countEachThreeElementsThatSumN23(v: IntArray, l: Int, r: Int, s: Int): Int {
+    var p = 0
+    var left = l
+    var right = r
+    mergeSort(v, l, r)
+    print(v)
+    for (i in 0..v.size - 1) {
+        val c = binarySearch(v, l, r, s - v[l] - v[r])
+        if (c != -1) p++
+    }
+    return p
+}
+
+//exercicio 3
+fun countInRange(v: IntArray, l: Int, r: Int, min: Int, max: Int): Int{
+    var p = 0
+    var min = l
+    var max = r
+    var mid = (l + r)/2
+    return 2
 }
 
 
-/*fun countInRange(v: IntArray, l: Int, r: Int, min: Int, max: Int): Int{
+fun main () {
+    //print(countPairsThatSumN(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 0, 7, 10))
+    //println(countEachThreeElementsThatSumN21(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 0, 7, 9))
+    println(countEachThreeElementsThatSumN22(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 0, 7, 9))
 
-}*/
-
-
-fun main (){
-    print(countPairsThatSumN(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 0, 7, 10))
-    //print(countEachThreeElementsThatSumN(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 0, 7, 9))
 }
