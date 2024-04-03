da059c0f13f363f20cc4e511b38993561f062da7
TiagoPraia <161584193+TiagoPraia@users.noreply.github.com>
2024-04-03 11:35:47 +0100

    Add files via upload

---
+++ src/main/kotlin/Quicksort.kt
@@ -0,0 +1,51 @@
+import java.util.Stack
+
+fun quickSort(array: Array<Int>, left: Int, right: Int) {
+    if (left<right) {
+        val i = partition(array, left, right)
+        quickSort(array, left, i - 1)
+        quickSort(array, i+1, right)
+    }
+}
+
+fun quickSortIterativo(array: Array<Int>, left: Int, right: Int) {
+    val stack = Stack<Int>()
+    stack.push(left)
+    stack.push(right)
+    while (!stack.isEmpty()) {
+        var r = stack.pop()
+        var l = stack.pop()
+        if (r <= l) continue
+        var i = partition(array, l, r)
+        if (l - i >= r - i) {
+            stack.push(l)
+            stack.push(i-1)
+        }
+        stack.push(i+1)
+        stack.push(r)
+        if (l - i < r - i) {
+            stack.push(l)
+            stack.push(i-1)
+        }
+    }
+}
+
+fun partition(array: Array<Int>, left:Int, right: Int): Int {
+    var i = left - 1
+    var j = right
+    val pivot = array[right]
+    while (true) {
+        while ( i < right && array[++i] < pivot);
+        while ( j > left && array[--j] > pivot);
+        if (i >= j) break
+        exchange(array, i, j)
+    }
+    exchange(array, i, right)
+    return i
+}
+
+fun exchange(a: Array<Int>, i: Int, j: Int) {
+    val tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+}
