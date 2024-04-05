25af6fef1ab71a8ee992082f289412ab965fc165
TiagoPraia <tiagobragancapraia@gmail.com>
2024-04-05 17:42:20 +0100

    pao

--- src/main/kotlin/Heap Sort.kt
+++ src/main/kotlin/Heap Sort.kt
@@ -1,6 +1,6 @@
-fun left(i: Int) = 2*i + 1
-fun right(i: Int) = 2*i + 2
-fun parent(i: Int) = (i - 1)/2
+private fun left(i: Int) = 2*i + 1
+private fun right(i: Int) = 2*i + 2
+private fun parent(i: Int) = (i - 1)/2
 
 private fun exchange(a: Array<Int>, i: Int, j: Int) {
     val tmp = a[i]
