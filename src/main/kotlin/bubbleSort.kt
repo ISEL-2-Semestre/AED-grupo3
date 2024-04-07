a2b436385f0b6e8cf7593a77250217c53482a9fd
TiagoPraia <161584193+TiagoPraia@users.noreply.github.com>
2024-04-07 18:42:48 +0100

    Update bubbleSort.kt

--- src/main/kotlin/bubbleSort.kt
+++ src/main/kotlin/bubbleSort.kt
@@ -1,8 +1,12 @@
 fun bubbleSort(a: IntArray, l: Int, r: Int){
     for (i in l until r){
+        var swapped = false
         for (j in r downTo i + 1){
-            if (a[j] < a[j-1]) exchange(a, j, j-1)
+            if (a[j] < a[j-1]) {
+                exchange(a, j, j-1)
+                swapped = true
+            }
         }
+        if (!swapped) break
     }
 }
-
